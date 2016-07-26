b2154ee9e8371ecbc9b54d40c9091e57f87faa4c
Lauren Kight <kight6@gmail.com>
2016-07-26 09:41:03 -0500

    added songs.js on nw branch and linked in html

---
+++ songs.js
@@ -0,0 +1,12 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+  songs.unshift("French Navy - by Camera Obscura on the album My Mauldin Career");
+  songs.push("Feeling OK - by Best Coast on the album California Nights");
+
+
