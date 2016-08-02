caa2dc905c037e1b0b081d96f6a7dc09989a3f5c
Lauren Kight <kight6@gmail.com>
2016-08-02 17:56:10 -0500

    added js files. added list and add html files and linked pages together.  additional styling to list.html

--- songs.js
+++ songs.js
@@ -9,4 +9,39 @@ songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little
   songs.unshift("French Navy - by Camera Obscura on the album My Mauldin Career");
   songs.push("Feeling OK - by Best Coast on the album California Nights");
 
+songs.forEach(outputSongs);
 
+function outputSongs(song) {
+
+    songs = song.replace('>', '-').replace(/[@/\(/!/\*/]/g,'');
+
+    // document.getElementById("songList").innerText += songs;
+    console.log(songs);
+}
+
+
+
+// In the navigation bar, make sure you have two links labeled "List Music", and "Add Music".
+
+// Add a DOM element that contains some input fields for the user to enter
+// in the name of a song, the artist for the song, and the album.
+// Add a <button> element at the bottom of the input fields labeled "Add".
+// The input fields and the add button make up the Add Music View.
+
+// The existing view - the combination of the filter form and the song list
+// - will be referred to as the List Music View.
+
+// The Add Music View should not appear when the user first visits your page.
+//  The song list with the corresponding filter form should be visible.
+
+// When the user clicks on "Add Music" in the navigation bar,
+//  the List Music View should be hidden, and the Add Music View
+//  should be shown (see example wireframe).
+
+// When the user clicks on "List Music" in the navigation bar,
+// the Add Music View should be hidden, and the List Music View should
+// be shown (see example wireframe).
+
+// Once the user fills out the song form and clicks the add button,
+// you should collect all values from the input fields,
+// add the song to your array of songs, and update the song list in the DOM.
